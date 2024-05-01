94d42c31c8dcece60e3ef594be6d4703f06abbd9
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-05-01 18:12:59 +0600

    Project With JS Event

--- script/main.js
+++ script/main.js
@@ -3,14 +3,65 @@ function productPrice(productId){
     const elementId= document.getElementById(productId);
     const priceString= elementId.innerText;
     const price= parseInt(priceString);
-    return price;
+    
+    // return price;
+
+    const totalValue=document.getElementById('total-price');
+
+    const totalValueString= totalValue.innerText;
+    const previousTotalValue= parseInt(totalValueString);
+
+    const totalPrice= previousTotalValue+price;
+    totalValue.innerText= totalPrice;
+    document.getElementById('total').innerText= totalPrice;
+
 }
 
+
+
+       document.getElementById('coupon-code').addEventListener('keyup',function(event){
+
+        const couponValue= event.target.value;
+
+        if(couponValue =='SELL200'){
+            document.getElementById("btn-coupon").removeAttribute('disabled');
+        }
+        
+       })
+        
+        document.getElementById('btn-coupon').addEventListener('click', function(){
+        const totalValue=document.getElementById('total-price');
+        const totalValueString= totalValue.innerText;
+        const previousTotalValue= parseInt(totalValueString);
+        const discount= previousTotalValue*0.2;
+
+        const grandTotal=previousTotalValue-discount;
+        document.getElementById('discount').innerText= discount;
+        document.getElementById('total').innerText= grandTotal;
+    
+
+         });
+        
+
+
 function product1(){
-      const product1Price=  productPrice('price');
+      const product1Price=  productPrice('price1');
 
       createElement('K. Accessories');
 }
+
+function product2(){
+
+    const product2Price= productPrice('price2');
+
+    createElement('K. Accessories 2')
+}
+function product3(){
+    const product2Price= productPrice('price2');
+    createElement('K. Accessories 2')
+}
+
+
 
 
 function createElement(productName){
@@ -28,6 +79,42 @@ function createElement(productName){
 
    
 }
+
+
+document.getElementById('btn-purchase').addEventListener('click',function(){
+    const totalValue=document.getElementById('total');
+    const totalValueString= totalValue.innerText;
+    const total= parseInt(totalValueString);
+
+    if(total==0){
+        document.getElementById('btn-purchase').setAttribute('disabled',true);
+    }
+    else{
+        document.getElementById('btn-purchase').removeAttribute('disabled');
+    }
+  
+    document.getElementById('total').innerText= 0;
+    document.getElementById('total-price').innerText= 0;
+    document.getElementById('discount').innerText= 0;
+    document.getElementById('coupon-code').value= '';
+    document.getElementById('btn-coupon').setAttribute('disabled',true);
+    document.getElementById('btn-purchase').setAttribute('disabled',true);
+
+    const modal = document.getElementById('my_modal_1');
+    modal.showModal();
+})
+
+
+function closeModal(){
+    const modal = document.getElementById('my_modal_1');
+    modal.close();
+    window.location.reload();
+}
+
+
+
+
+
 
 
 
